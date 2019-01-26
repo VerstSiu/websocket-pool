1e5d875bb7e93c5c393670f14ed4a43579a32230
verstsiu <verstsiu@126.com>
2019-01-26 15:38:35 +0800

    :sparkles: Add http connection support

---
+++ http-okhttp/src/main/kotlin/com/ijoic/data/source/http/okhttp/HttpConnection.kt
@@ -0,0 +1,106 @@
+/*
+ *
+ *  Copyright(c) 2019 VerstSiu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.ijoic.data.source.http.okhttp
+
+import com.ijoic.data.source.ConnectionListener
+import com.ijoic.data.source.context.ExecutorContext
+import com.ijoic.data.source.context.impl.DefaultExecutorContext
+import com.ijoic.data.source.impl.BaseConnection
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+/**
+ * Http connection
+ *
+ * @author verstsiu created at 2019-01-26 10:00
+ */
+class HttpConnection(
+  private val api: String,
+  private val context: ExecutorContext = DefaultExecutorContext): BaseConnection(context) {
+
+  private var stateListener: ConnectionListener? = null
+  private var failMessage: Any? = null
+  private var httpCall: Call? = null
+
+  override var isActive: Boolean = false
+    private set
+
+  override fun onRelease() {
+    stateListener = null
+    failMessage = null
+    isActive = false
+    httpCall?.checkAndCancel()
+    httpCall = null
+  }
+
+  override fun prepare(listener: ConnectionListener) {
+    stateListener = listener
+    val oldFailMessage = failMessage
+
+    if (oldFailMessage != null) {
+      send(oldFailMessage)
+    } else {
+      isActive = true
+      listener.onConnectionComplete()
+    }
+  }
+
+  override fun send(message: Any) {
+    failMessage = null
+    context.io { doSendMessage(message) }
+  }
+
+  private fun doSendMessage(message: Any) {
+    var httpCall: Call? = null
+
+    try {
+      val request = Request.Builder()
+        .url(api)
+        .build()
+
+      httpCall = client.newCall(request)
+      this.httpCall = httpCall
+
+      val response = httpCall.execute()
+      val responseText = response.body()?.string()
+      this.httpCall = null
+
+      if (responseText != null) {
+        dispatchReceivedMessage(responseText)
+      }
+
+    } catch (t: Throwable) {
+      if (this.httpCall == httpCall) {
+        isActive = false
+        failMessage = message
+        stateListener?.onConnectionFailure(t)
+      }
+    }
+  }
+
+  companion object {
+    private val client by lazy { OkHttpClient() }
+
+    private fun Call.checkAndCancel() {
+      if (this.isExecuted && !this.isCanceled) {
+        this.cancel()
+      }
+    }
+  }
+}
