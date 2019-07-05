4bd97a3d0e528f82a3b51f0e241366196fbda267
verstsiu <verstsiu@126.com>
2019-07-05 21:40:24 +0800

    :sparkles: Add ReadChannel support

---
+++ core/src/main/kotlin/com/ijoic/data/source/channel/impl/ReadChannel.kt
@@ -0,0 +1,45 @@
+package com.ijoic.data.source.channel.impl
+
+import com.ijoic.data.source.Connection
+import com.ijoic.data.source.handler.MessageHandler
+import com.ijoic.data.source.pool.ConnectionPool
+
+/**
+ * Read channel
+ *
+ * @author verstsiu created at 2019-07-05 21:31
+ */
+class ReadChannel(
+  private val pool: ConnectionPool,
+  private val handler: MessageHandler): BaseChannel() {
+
+  private var bindConnection: Connection? = null
+
+  private val editLock = Object()
+
+  private val connectionListener = object: ConnectionPool.ConnectionChangedListener {
+    override fun onConnectionActive(connection: Connection) {
+      synchronized(editLock) {
+        bindConnection = connection
+        connection.addMessageHandler(handler)
+      }
+    }
+
+    override fun onConnectionInactive(connection: Connection) {
+      synchronized(editLock) {
+        if (connection == bindConnection) {
+          bindConnection = null
+          connection.removeMessageHandler(handler)
+        }
+      }
+    }
+  }
+
+  override fun release() {
+    synchronized(editLock) {
+      pool.removeConnectionChangeListener(connectionListener)
+      bindConnection?.removeMessageHandler(handler)
+      bindConnection = null
+    }
+  }
+}
